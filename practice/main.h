7ae48d27cd92109a35716e1d539d0fafabd30b87
pilusx <poteka@wp.pl>
2020-02-11 16:10:08 +0100

    Main

--- practice/main.h
+++ practice/main.h
@@ -1,11 +1,34 @@
 #include <iostream>
+#include <vector>
 
 #include "io.h"
 
 void real_main() {
-    OUT << "Example solution" << std::endl;
-    std::string x;
-    while(IN >> x) {
-        OUT << x;
+    // IN
+    int M, N;
+    std::vector<int> sizes;
+
+    IN >> M >> N;
+    sizes.resize(N);
+    for (int i = 0; i < N; i++) {
+        IN >> sizes[i];
     }
+
+    // OUT
+    int d = 0;
+    std::vector<int> slices;
+
+    long long int sum = 0;
+    for(int i = 0; i < N; i++) {
+        if(d + sizes[i] <= M) {
+            d += sizes[i];
+            slices.push_back(i);
+        }
+    }
+
+    OUT << slices.size() << std::endl;
+    for(auto& i : slices) {
+        OUT << i << " ";
+    }
+    OUT << std::endl;
 }
