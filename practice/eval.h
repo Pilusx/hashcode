70ce0270226180e3fd1cb4610e200cdb1a062053
pilusx <poteka@wp.pl>
2020-02-11 16:03:18 +0100

    Eval

--- practice/eval.h
+++ practice/eval.h
@@ -1,12 +1,43 @@
 #include <iostream>
+#include <vector>
 
 #include "io.h"
 
 int64_t real_eval() {
-    std::string x1, x2;
-    int64_t score = 271743;
-    IN >> x1;
-    IN2 >> x2;
+    // IN
+    int M, N;
+    std::vector<int> sizes;
 
-    return score;
+    // IN2
+    int K;
+    std::vector<int> choice;
+
+    IN >> M >> N;
+    sizes.resize(N);
+    for (int i = 0; i < N; i++) {
+        IN >> sizes[i];
+    }
+
+    IN2 >> K;
+    choice.resize(K);
+    for (int i = 0; i < K; i++) {
+        IN2 >> choice[i];
+    }
+
+    if(!(0 <= K && K <= N)) {
+        std::cerr << "Invalid K" << std::endl;
+        return -1;
+    }
+
+    long long int sum = 0;
+    for(auto& slice : choice) {
+        sum += sizes[slice];
+    }
+
+    if(sum > M) {
+        std::cerr << "Invalid sum" << std::endl;
+        return -1;
+    }
+
+    return sum;
 }
