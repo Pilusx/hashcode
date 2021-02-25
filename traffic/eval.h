27c3cc92a181ef83ab5f2c08ea5caf2f4c1a957d
pilusx <poteka@wp.pl>
2021-02-25 20:14:48 +0100

    traffic: partial eval

--- traffic/eval.h
+++ traffic/eval.h
@@ -3,10 +3,23 @@
 #include "structs.h"
 
 int64_t real_eval() {
-    std::string x1, x2;
-    int64_t score = 271743;
-    IN >> x1;
-    IN2 >> x2;
+    read_input();
+    read_output();
+
+    // Init
+    state_init();
+
+    int64_t score = 0;
+
+    while(!g_light_events.empty()) {
+        GreenLight light = g_light_events.top();
+        g_light_events.pop();
+
+    
+
+
+
+    }
 
     return score;
 }
--- traffic/structs.h
+++ traffic/structs.h
@@ -1,3 +1,5 @@
+#include <list>
+#include <queue>
 #include <vector>
 #include <unordered_map>
 
@@ -13,21 +15,24 @@ struct ParametersIn {
     int F; // 10^3
 };
 
+typedef int CarId;
+typedef int IntersectionId;
 typedef int StreetId;
 
 struct Street {
-    int B, E; // 0 <= B, E, < I
+    IntersectionId B, E; // 0 <= B, E, < I
     StreetId name;
     int L; // L <= D
 };
 
 struct Path {
     int P; // 10^3
-    std::vector<StreetId> streets;
+    std::list<StreetId> streets;
 };
 
 ParametersIn g_input_parameters;
 std::vector<Street> g_streets;
+std::vector<std::vector<StreetId>> g_neighbours; // IntersectionId -> [StreetId]
 std::vector<std::string> g_street_names;
 std::unordered_map<std::string, StreetId> g_street_names_inv;
 std::vector<Path> g_paths;
@@ -36,10 +41,11 @@ std::vector<Path> g_paths;
 struct GreenLight {
     StreetId street_name;
     int T;
+    int T_start;
 };
 
 struct Intersection {
-    int i;
+    IntersectionId i;
     int Ei;
     std::vector<GreenLight> lights;
 };
@@ -51,25 +57,28 @@ struct Submission {
 
 Submission g_submission;
 
-
 void read_input() {
     ParametersIn &p = g_input_parameters;
     IN >> p.D >> p.I >> p.S >> p.V >> p.F;
 
     g_streets.resize(p.S);
     g_street_names.resize(p.S);
+    g_neighbours.resize(p.I);
     for(int i = 0; i < p.S; i++) {
-        auto& street = g_streets[i];
-        auto& name = g_street_names[i];
+        Street street;
+        std::string name;
         IN >> street.B >> street.E >> name >> street.L;
         street.name = i;
+        g_streets[i] = street;
+        g_neighbours[street.B].push_back(i);
+        g_street_names[i] = name;
         g_street_names_inv[name] = i;
     }
-    
+
     g_paths.resize(p.V);
     for(auto& path : g_paths) {
         IN >> path.P;
-        path.streets.resize(path.P);
+        path.streets.push_back(path.P);
         for(auto& streetid : path.streets) {
             std::string name;
             IN >> name;
@@ -99,6 +108,41 @@ void write_output() {
         OUT << schedule.Ei << std::endl;
         for(auto & light : schedule.lights) {
             OUT << g_street_names[light.street_name] << " " << light.T << std::endl;
+        }
+    }
+}
+
+// State
+
+typedef int Time;
+
+std::unordered_map<IntersectionId, StreetId> g_lights_on;
+std::unordered_map<IntersectionId, std::unordered_map<StreetId, std::queue<CarId>>> g_intersection_state;
+std::unordered_map<StreetId, std::unordered_map<CarId, Time>> g_street_state;
+std::priority_queue<GreenLight> g_light_events;
+
+bool operator<(const GreenLight& lhs, const GreenLight& rhs) {
+    return lhs.T_start < rhs.T_start 
+        || (lhs.T_start == rhs.T_start && lhs.street_name < rhs.T_start);    
+}
+
+void state_init() {
+    for (CarId car = 0; car < g_paths.size(); car++) {
+        Path& path = g_paths[car];
+        StreetId begin = path.streets.front();
+        path.streets.pop_front();
+        IntersectionId start = g_streets[begin].E;
+        g_intersection_state[start][begin].push(car);
+    }
+
+    for(int i = 0; i < g_submission.A; i++) {
+        Intersection& intersection = g_submission.intersections[i];
+        g_lights_on[intersection.i] = intersection.lights[0].street_name;
+        Time T_current = 0;
+        for(auto & light : intersection.lights) {
+            light.T_start = T_current;
+            T_current += light.T;
+            g_light_events.push(light);
         }
     }
 }
