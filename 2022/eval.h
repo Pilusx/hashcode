bb8622b84e32ec0ceaf387f78af3770aec8e2c83
pilusx <poteka@wp.pl>
2022-02-24 20:27:37 +0100

    Eval

--- 2022/eval.h
+++ 2022/eval.h
@@ -11,7 +11,8 @@ int64_t real_eval() {
     int time = 0;
 
     for(auto& ass : assignments) {
-        Project p = projects[ass.project_name];
+        std::string name = ass.project_name;
+        Project p = projects[name];
 
         std::map<std::string, std::pair<int, int>> sanity_levels;
         for(auto& skill : p.skill_levels) {
@@ -22,13 +23,13 @@ int64_t real_eval() {
             std::string skill = ass.skill_assignees[i].first;
             std::string contributor = ass.skill_assignees[i].second;
 
-            // Upgrade skills
+            // Update max skill level
 
             auto& skills = contributors[contributor].skills;
-            for(auto& skill : sanity_levels) {
-                auto it = skills.find(skill.first);
+            for(auto& slevel : sanity_levels) {
+                auto it = skills.find(slevel.first);
                 if(it != skills.end()) {
-                    it->first = std::max(it->first, skill.second); // Update max skill level
+                    slevel.second.second = std::max(slevel.second.second, it->second);
                 } 
             }
 
@@ -36,18 +37,43 @@ int64_t real_eval() {
             int contributor_level = (it != skills.end() ? it->second : 0);
 
             if(skill != p.skill_levels[i].first) {
-                OUT << "Skill order mismatch " << skill << "!=" << p.skill_levels[i].first << "(in " << ass.project_name;
+                OUT << "Skill order mismatch " << skill << "!=" << p.skill_levels[i].first << "(in " << name;
                 exit(1);
             }
             if (contributor_level + 1 < p.skill_levels[i].second) {
-                OUT << "Skill is too low for " << ass.project_name << "/" << skill <<"... " << contributor_level << "<<" << p.skill_levels[i].second;
+                OUT << "Skill is too low for " << name << "/" << skill <<"... " << contributor_level << "<<" << p.skill_levels[i].second;
                 exit(1);
             }
         }
+
+        for(auto slevel : sanity_levels) {
+            if(slevel.second.first < slevel.second.second) {
+                OUT << "Skill not masted "  << name << "/" << slevel.first << "... " << slevel.second.first << "<<" << slevel.second.second;
+                exit(1);
+            }
+        }
+
         // Seems ok....
+        time += p.time_to_complete;
+        int dpoints = std::max(0, p.reward - (p.best_before_day - time));
+        score += dpoints;
 
+        // Upgrade skills...
+        for(auto &skass : ass.skill_assignees) {
+            std::string skill_name = skass.first;
+            std::string contributor = skass.second;
+            int required_level = sanity_levels[skill_name].second;
+            auto& skills = contributors[contributor].skills;
+            auto it = skills.find(skill_name);
+            if(it != skills.end()) {
+                if(it->second <= required_level) {
+                    it->second++;
+                }
+            }
 
-        projects.erase(ass.project_name);
+        }
+
+        projects.erase(name);
     }
     return score;
 }
